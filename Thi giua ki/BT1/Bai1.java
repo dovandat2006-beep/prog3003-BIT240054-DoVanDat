1ff0ac29dcaad6f2a5f32bec2939c702b79bc24e
dovandat2006-beep <dovandat2006@gmail.com>
2026-03-03 09:47:59 +0700

    Refactor student class and improve structure

--- Thi giua ki/BT1/Bai1.java
+++ Thi giua ki/BT1/Bai1.java
@@ -1,68 +1,78 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.bai1;
 
-public class student {
-        private string id;
-        private string name;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bai1 {
+    public static class Student {
+        private String id;
+        private String name;
         private double gpa;
-}
-public student(String id, String name, double gpa) {
-    this.id = id;
-    this.name = name;
-    this. gpa = gpa;
-}
-public String getId(String id) {
-  return id;
-}
-public void setId(String id) {
-    this.id = id;
-}
-public String getName() {
-    return name;
-}
-public void setName(String name) {
-    this.name = name;
-}
-public double getGpa() {
-    return gpa;
-}
-public void setGpa(double gpa) {
-    this.gpa = gpa;
-}
-@Override
-public String toString() {
-    return "student{" +
-            "id='" + id + '\''' +
-            ",name='" + '\''
-            ", gpa="
-            '}';
-            
-}
-import.java.util.Arraylist;
-import.java.util.List;
-public class StudentManager<T> {
-    private List<T> data;
-    public StudentManager() {
-        data = new ArrayList<>();
+
+        public Student(String id, String name, double gpa) {
+            this.id = id;
+            this.name = name;
+            this.gpa = gpa;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public double getGpa() {
+            return gpa;
+        }
+
+        public void setGpa(double gpa) {
+            this.gpa = gpa;
+        }
+
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "id='" + id + '\'' +
+                    ", name='" + name + '\'' +
+                    ", gpa=" + gpa +
+                    '}';
+        }
     }
-    public void add(T item) {
-        data.add(item);
+    public static class StudentManager<T> {
+        private List<T> data;
+
+        public StudentManager() {
+            data = new ArrayList<>();
+        }
+
+        public void add(T item) {
+            data.add(item);
+        }
+
+        public List<T> getAll() {
+            return data;
+        }
     }
-    public List<T> getALl() {
-        return data;
-    }
-}
-public class main {
     public static void main(String[] args) {
+
         StudentManager<Student> manager = new StudentManager<>();
-        manager.add(new Student("SV01", "trang dang khoi", "7.8"));
-        manager.add(new Student("Sv02", "nguyen doan huy", "9.8"));
-        manager.add(new Student("SV03", "do van dat", "10"));
-        for (Student s : manager.getALl()) {
-            system.out.printLn(s);
+
+        manager.add(new Student("SV01", "Trang Dang Khoi", 7.8));   // gpa = double
+        manager.add(new Student("SV02", "Nguyen Doan Huy", 9.8));
+        manager.add(new Student("SV03", "Do Van Dat", 10.0));
+
+        for (Student s : manager.getAll()) {
+            System.out.println(s);
         }
     }
 }
